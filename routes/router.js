df2eaa464eba58297228897c0ae1d561ea578e6b
Giovanni Gabriel Septriadi <giovannigs@Giovannis-MacBook-Air.local>
2022-09-15 12:32:16 +0700

    done form add

--- routes/router.js
+++ routes/router.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const Controller = require("../controllers/controllers");
+const ProductController = require("../controllers/ProductControllers");
 const UserController = require("../controllers/UserController");
 
 // GET Register
-router.get("/register", UserController.registerForm)
+router.get("/register", UserController.registerForm);
 // POST Register
-router.post("/register", UserController.postRegister)
+router.post("/register", UserController.postRegister);
 
 // GET Register
-router.get("/login", UserController.loginForm)
+router.get("/login", UserController.loginForm);
 // POST Register
-router.post("/login", UserController.postLogin)
+router.post("/login", UserController.postLogin);
 
-router.get("/add", Controller.formAdd);
-router.post("/add", Controller.createProduct);
+router.get("/product/add", ProductController.formAdd);
+router.post("/product/add", ProductController.createProduct);
 
 module.exports = router;
