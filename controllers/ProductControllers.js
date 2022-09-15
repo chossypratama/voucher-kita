c3f4fc23bbb3f98149982c5ac8c6b33445d6afa6
Giovanni Gabriel Septriadi <giovannigs@Giovannis-MacBook-Air.local>
2022-09-15 14:15:03 +0700

    edit product dan delete product

--- controllers/ProductControllers.js
+++ controllers/ProductControllers.js
@@ -13,7 +13,7 @@ class Controller {
 
   static createProduct(req, res) {
     const { name, description, price, imageUrl, stock, CategoryId } = req.body;
-    let UserId = 8; //<<<UserId 8 sebagai Seller
+    let UserId = req.params.userId;
     Product.create({
       name,
       description,
@@ -24,7 +24,72 @@ class Controller {
       UserId,
     })
       .then(() => {
-        res.redirect("/add");
+        res.redirect(`/product/${UserId}/add`);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  }
+
+  static listProduct(req, res) {
+    Product.findAll({
+      include: [User, Category],
+      where: {
+        UserId: +req.params.userId,
+      },
+    })
+      .then((result) => {
+        // res.send(result);
+        res.render("listProduct", { products: result });
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  }
+
+  static deleteProduct(req, res) {
+    Product.destroy({
+      where: {
+        id: +req.params.productId,
+      },
+    })
+      .then(() => {
+        res.redirect(`/product/${+req.params.userId}`);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  }
+
+  static formEditProduct(req, res) {
+    let product;
+    Product.findByPk(+req.params.productId, {
+      include: Category,
+    })
+      .then((result) => {
+        product = result;
+        return Category.findAll();
+      })
+      .then((result) => {
+        res.render("formEditProduct", { category: result, product });
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  }
+
+  static updateProduct(req, res) {
+    const { name, description, price, imageUrl, stock, CategoryId } = req.body;
+    Product.update(
+      { name, description, price, imageUrl, stock, CategoryId },
+      {
+        where: {
+          id: +req.params.productId,
+        },
+      }
+    )
+      .then(() => {
+        res.redirect(`/product/${+req.params.userId}`);
       })
       .catch((err) => {
         res.send(err);
